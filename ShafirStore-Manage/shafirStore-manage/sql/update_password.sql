9cbd542b1ffe034b3868cc4eccd626bf818cafe5
赖伟彬 <2912366002@qq.com>
2026-03-25 23:15:12 +0800

    feat：更改登录的样式

--- ShafirStore-Manage/shafirStore-manage/sql/update_password.sql
+++
@@ -1,3 +0,0 @@
--- 更新用户密码（密码123456的正确BCrypt哈希）
-UPDATE sys_user SET password = '$2a$10$znNmsDG6DOY0hPUpZerPNuMM/JwSRyprt20UPN3yo1g4j19qPUOIKK' WHERE username = 'admin';
-UPDATE sys_user SET password = '$2a$10$znNmsDG6DOY0hPUpZerPNuMM/JwSRyprt20UPN3yo1g4j19qPUOIKK' WHERE username = 'zhangsan';
